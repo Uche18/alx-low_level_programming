44b3450d0a03a239ada754e6c1e747ff272a7502
Uche18 <Ucheemmanuel003@gmail.com>
2022-03-17 05:32:25 -0700

    Task 3 commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code for alx School students.
+ * Return: void .
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar (i + '0');
+}
+_putchar('\n');
+}
