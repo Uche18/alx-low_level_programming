716d6a862a430601d0ef1ce702c303752a50380e
Uche18 <Ucheemmanuel003@gmail.com>
2022-03-22 06:05:26 -0700

    Task 3 commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,19 @@
-#include "holberton.h"
-#include <string.h>
+#include "main.h"
 
 /**
- * _puts - check the code for Holberton School students.
- * @str: variable.
+ * _puts - Write a function that prints a string, followed
+ * by a new line, to stdout.
+ * @str: an input string
+ * Return: Nothing
  */
 void _puts(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-_putchar('\n');
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
