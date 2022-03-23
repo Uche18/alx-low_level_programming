88dbff4f08f9577c799a59d65db5e6313a4abf6e
Uche18 <Ucheemmanuel003@gmail.com>
2022-03-23 11:16:23 -0700

    Task 2 commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copys the strings.
+ * @dest: an input string
+ * @src: an input string
+ * @n: an input integer
+ * Return: A pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for ( ; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; n > i; ++i)
+		dest[i] = '\0';
+
+	return (dest);
+}
