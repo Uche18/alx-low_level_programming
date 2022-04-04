5bee26c3d6b54d45210f16e593bc85c119f36f69
Uche18 <Ucheemmanuel003@gmail.com>
2022-04-04 04:08:20 -0700

    task 6 commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+int is_divisible(int num, int div);
+
+/**
+ * is_prime_number - a function that returns 1 if the input integer is
+ * a prime number, otherwise return 0.
+ * @n: an input integer
+ * Return: 1 if n is prime or 0 in otherwise
+ */
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n <= 1)
+		return (0);
+
+	if (n <= 3)
+		return (1);
+
+	return (is_divisible(n, div));
+}
+
+/**
+ * is_divisible - check if num is divisible
+ * @num: the number to be checked
+ * @div: the result of division
+ * Return: 1 if num is divisible, or 0 if num is not divisible
+ */
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+
+	if (div == num / 2)
+		return (1);
+
+	return (is_divisible(num, div + 1));
+}
