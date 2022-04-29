3b01b24192993daf29dfea6b06dea7f1042b7074
Uche18 <Ucheemmanuel003@gmail.com>
2022-04-29 11:16:46 -0700

    task 0 commit

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*
+ * File: main.h
+ * AUth: Abraham Opiko
+ * Desc: Header file containing prototypes for all functions
+ * in the 0x14-bit_manipulation directory.
+ */
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif /* _MAIN_H_ */
